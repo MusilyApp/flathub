0f8bc98fbe09c7d18cc68ad9ae8e2bed46177562
FelipeYslaoker <felipeyslaok@gmail.com>
2025-04-27 18:56:00 -0300

    added: tray icon to system icons

--- assets/setup_musily.sh
+++ assets/setup_musily.sh
@@ -44,3 +44,11 @@ cp -r $projectId.desktop $desktopFileDir/
 metadataDir=/app/share/metainfo
 mkdir -p $metadataDir
 cp -r $projectId.metainfo.xml $metadataDir/
+
+# Copy tray icon(s) to icon theme directory for system tray
+mkdir -p /app/share/icons/hicolor/32x32/apps
+ls -l $projectName/data/flutter_assets/assets/icons/musily_tray/
+cp $projectName/data/flutter_assets/assets/icons/tray/tray_icon.png /app/share/icons/hicolor/32x32/apps/musily_tray_icon.png
+
+# Update icon cache
+gtk-update-icon-cache /app/share/icons/hicolor
