71572c1d4f47b9b305ce3b1a95e6a94fbf75c455
FelipeYslaoker <felipeyslaok@gmail.com>
2025-04-27 19:38:18 -0300

    fixes

--- assets/setup_musily.sh
+++ assets/setup_musily.sh
@@ -48,7 +48,7 @@ cp -r $projectId.metainfo.xml $metadataDir/
 # Copy tray icon(s) to icon theme directory for system tray
 mkdir -p /app/share/icons/hicolor/32x32/apps
 ls -l $projectName/data/flutter_assets/assets/icons/musily_tray/
-cp $projectName/data/flutter_assets/assets/icons/tray/tray_icon.png /app/share/icons/hicolor/32x32/apps/musily_tray_icon.png
+cp $projectName/data/flutter_assets/assets/icons/tray/tray_icon.png /app/share/icons/hicolor/32x32/apps/musily_tray.png
 
 # Update icon cache
 gtk-update-icon-cache /app/share/icons/hicolor
